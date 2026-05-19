d04f88d9ef69387b1d9111df15c65eafaa40c039
Xebec51 <rinaldi.ruslan51@gmail.com>
2026-05-19 10:50:41 +0800

    Add Google Analytics and fix CSS types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import AOSInit from "../components/AOSInit";
 import Preloader from "../components/Preloader";
@@ -16,6 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id">
+      <head>
+        {/* Google Analytics - set NEXT_PUBLIC_GA_ID in environment */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', { page_path: window.location.pathname });`}
+        </Script>
+      </head>
       <body className="antialiased">
         <Preloader />
         <AOSInit />
